0b2af06ad4b516ed3bb48b918c9f7c2ea4bb802c
godfreypj <godfreypj@outlook.com>
2020-07-21 15:38:16 +1000

    Finalized functionality of displaying the info in a pop-up window and image in main window.  Included a button to restart program/main menu

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import scraper
+from PIL import Image, ImageTk
 import tkinter as tk
+from tkinter import PhotoImage, Canvas
+import io
+import urllib
+import base64
 
-if __name__ == "__main__":
-
-    #Create window for user to choose region
+def main():
+#Create window for user to choose region
     main_window = tk.Tk()
     main_window.geometry("600x400")
     main_window.title("Cyclone Scraper")
@@ -14,13 +18,63 @@ if __name__ == "__main__":
     title_lbl.pack()
     title_lbl2.pack()
 
+    #function to call the appropriate method in scaper module
+    def callScraper(selection):
+        if selection == 1:
+            data = scraper.atlc()
+            popup(data)
+        if selection == 2:
+            data = scraper.epac()
+            popup(data)
+        if selection == 3:
+            data = scraper.cpac()
+            popup(data)
+    
+    #function to call popupwindow to display info to user
+    def popup(data):
+        #Method to restart program and close existing windows
+        def restart():
+            main_window.destroy()
+            popup_window.destroy()
+            main()
+            
+        #Popup window to display info to user
+        popup_window = tk.Tk()
+        popup_window.geometry("500x300")
+        popup_window.title("Active Cyclones")
+        title_lbl = tk.Label(popup_window, text=data[0])
+        longlat_lbl = tk.Label(popup_window, text=data[1])
+        area_lbl = tk.Label(popup_window, text=data[2])
+        active_label = tk.Label(popup_window, text=data[3])
+        
+        #Button to restart program
+        end_btn = tk.Button(popup_window, text="Main Menu", command=lambda: restart())
+        end_btn.place(x=170, y=250)
+
+        #Grab image URL and display on popup window
+        raw_data = urllib.request.urlopen(data[4]).read()
+        image = base64.encodebytes(raw_data)
+        im = tk.PhotoImage(data=image)
+        cv = tk.Canvas(bg="white")
+        cv.pack(side='top', fill='both', expand='yes')
+        cv.create_image(10,10,image=im, anchor="nw")
+        title_lbl.pack()
+        longlat_lbl.pack()
+        area_lbl.pack()
+        active_label.pack()
+        popup_window.mainloop()
+
     #Buttons for selection
-    atlc_btn = tk.Button(main_window, text="Atlantic", command=lambda: scraper.scraper(3))
-    epac_btn = tk.Button(main_window, text="Eastern Pacific", command=lambda: scraper.scraper(2))
-    cpac_btn = tk.Button(main_window, text="Central Pacific", command=lambda: scraper.scraper(1))
+    atlc_btn = tk.Button(main_window, text="Atlantic", command=lambda: callScraper(1))
+    epac_btn = tk.Button(main_window, text="Eastern Pacific", command=lambda: callScraper(2))
+    cpac_btn = tk.Button(main_window, text="Central Pacific", command=lambda: callScraper(3))
     atlc_btn.place(x=270, y=150)
     epac_btn.place(x=255, y=200)
     cpac_btn.place(x=255, y=250)
-    
+
     #Start main loop
     main_window.mainloop()
+
+
+if __name__ == "__main__":
+    main()
--- scraper.py
+++ scraper.py
@@ -4,56 +4,53 @@ from bs4 import BeautifulSoup
 #The class to retrieve data from the NOAA website.  Depending on the users selection,
 #  this will either retrieve data from Central, Eastern Pacific or Atlantic.  The args
 #  for the class are default "self" and the "selection", which is an int (for now)
-class scraper:
-    def __init__(self, selection):
-        self.selection = int(selection)
-        if selection == 1:
-            page = requests.get("https://www.nhc.noaa.gov/?cpac")
-            soup = BeautifulSoup(page.content, 'html.parser')
-            imagePage = requests.get("https://www.nhc.noaa.gov/gtwo.php?basin=cpac&fdays=2")
-            imageSoup = BeautifulSoup(page.content, 'html.parser')
-            for text in soup.select('a[href="/cyclones/?cpac"]'):
-                title = text.get_text()
-            areaTitle = soup.select("th span")[3].get_text()
-            areaLongLat =  soup.select("th span")[4].get_text()
-            activeStorms = soup.find_all(class_='hdr')[2].get_text()
-            stormImage = imageSoup.find("img", {"id": "twofig0d"})
-            image = "https://www.nhc.noaa.gov" + stormImage["src"][:-7]
-            print("Title: ", title)
-            print("Area: ", areaTitle)
-            print("Long/Lat: ", areaLongLat)
-            print("Storms: ", activeStorms)
-            print("Image: ", image)
-        if selection == 2:
-            page = requests.get("https://www.nhc.noaa.gov/?epac")
-            soup = BeautifulSoup(page.content, 'html.parser')
-            imagePage = requests.get("https://www.nhc.noaa.gov/gtwo.php?basin=epac&fdays=2")
-            imageSoup = BeautifulSoup(page.content, 'html.parser')
-            for text in soup.select('a[href="/cyclones/?epac"]'):
-                title = text.get_text()
-            areaTitle = soup.select("th span")[1].get_text()
-            areaLongLat = soup.select("th span")[2].get_text()
-            activeStorms = soup.find_all(class_='hdr')[1].get_text()
-            stormImage = imageSoup.find("img", {"id": "twofig0d"})
-            image = "https://www.nhc.noaa.gov" + stormImage["src"][:-7]
-            print("Title: ", title)
-            print("Area: ", areaTitle)
-            print("Long/Lat: ", areaLongLat)
-            print("Storms: ", activeStorms)
-            print("Image: ", image)
-        if selection == 3:
-            page = requests.get("https://www.nhc.noaa.gov/?atlc")
-            soup = BeautifulSoup(page.content, 'html.parser')
-            imagePage = requests.get("https://www.nhc.noaa.gov/gtwo.php?basin=atlc&fdays=2")
-            imageSoup = BeautifulSoup(page.content, 'html.parser')
-            for text in soup.select('a[href="/cyclones/"]'):
-                title = text.get_text()
-            areaTitle = soup.select("th span")[0].get_text()
-            activeStorms = soup.find_all(class_='hdr')[0].get_text()
-            stormImage = imageSoup.find("img", {"id": "twofig0d"})
-            image = "https://www.nhc.noaa.gov" + stormImage["src"][:-7]
-            print("Title: ", title)
-            print("Area: ", areaTitle)
-            print("Storms: ", activeStorms)
-            print("Image: ", image)
+
+    
+def atlc():
+    page = requests.get("https://www.nhc.noaa.gov/?atlc")
+    soup = BeautifulSoup(page.content, 'html.parser')
+    imagePage = requests.get("https://www.nhc.noaa.gov/gtwo.php?basin=atlc&fdays=2")
+    imageSoup = BeautifulSoup(page.content, 'html.parser')
+    for text in soup.select('a[href="/cyclones/"]'):
+        title = text.get_text()
+    areaTitle = soup.select("th span")[0].get_text()
+    activeStorms = soup.find_all(class_='hdr')[0].get_text()
+    stormImage = imageSoup.find("img", {"id": "twofig0d"})
+    image = "https://www.nhc.noaa.gov" + stormImage["src"][:-7]
+    data = [title, "No Long/Lat Available", areaTitle, activeStorms, image]
+    return data
+
+def epac():
+    page = requests.get("https://www.nhc.noaa.gov/?epac")
+    soup = BeautifulSoup(page.content, 'html.parser')
+    imagePage = requests.get("https://www.nhc.noaa.gov/gtwo.php?basin=epac&fdays=2")
+    imageSoup = BeautifulSoup(page.content, 'html.parser')
+    for text in soup.select('a[href="/cyclones/?epac"]'):
+        title = text.get_text()
+    areaTitle = soup.select("th span")[1].get_text()
+    areaLongLat = soup.select("th span")[2].get_text()
+    activeStorms = soup.find_all(class_='hdr')[1].get_text()
+    stormImage = imageSoup.find("img", {"id": "twofig0d"})
+    image = "https://www.nhc.noaa.gov" + stormImage["src"][:-7]
+    data = [title, areaTitle, areaLongLat, activeStorms, image]
+    return data
+
+def cpac():
+    page = requests.get("https://www.nhc.noaa.gov/?cpac")
+    soup = BeautifulSoup(page.content, 'html.parser')
+    imagePage = requests.get("https://www.nhc.noaa.gov/gtwo.php?basin=cpac&fdays=2")
+    imageSoup = BeautifulSoup(page.content, 'html.parser')
+    for text in soup.select('a[href="/cyclones/?cpac"]'):
+        title = text.get_text()
+    areaTitle = soup.select("th span")[3].get_text()
+    areaLongLat =  soup.select("th span")[4].get_text()
+    activeStorms = soup.find_all(class_='hdr')[2].get_text()
+    stormImage = imageSoup.find("img", {"id": "twofig0d"})
+    image = "https://www.nhc.noaa.gov" + stormImage["src"][:-7]
+    data = [title, areaTitle, areaLongLat, activeStorms, image]
+    return data
+
+
+
+
             
